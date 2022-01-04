f3b6243f34af17f0d99d2932a357a1b7f051deea
byhow <baihaow@uci.edu>
2022-01-03 22:09:52 -0800

    chore: init

---
+++ README.md
@@ -0,0 +1,21 @@
+# @byhow/eslint-config
+
+[![npm](https://img.shields.io/npm/v/@byhow/eslint-config)](https://npmjs.com/package/@byhow/eslint-config)
+
+## Usage
+
+### Install
+
+```bash
+pnpm add -D eslint @byhow/eslint-config
+```
+
+### Config `.eslintrc`
+
+```json
+{
+  "extends": [
+    "@byhow"
+  ]
+}
+```
---
+++ packages/base/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+  ],
+  rules: {
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
+  }
+}
