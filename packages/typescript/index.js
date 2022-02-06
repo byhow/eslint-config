a8969c52fcfe0e83950082b9bc376c3d1710c4df
byhow <baihaow@uci.edu>
2022-02-05 22:08:37 -0800

    chore: revert workspace config

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,6 +1,9 @@
+const basic = require('@byhow/eslint-config-base');
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     '@byhow/eslint-config-base'
-  ]
+  ],
+  override: basic.overrides
 }
